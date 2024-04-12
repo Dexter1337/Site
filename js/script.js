1aeed9e2ffa65b19ada26a6576102e8ee7cec2e1
Alex <44549259+Dexter1337@users.noreply.github.com>
2024-04-12 22:56:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ window.addEventListener("scroll", function()
 {
 	header.classList.toggle("sticky",window.scrollY > 60)
 });
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+//каруселька
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
